01c1dfc864ba8412caf378b9ee2cb94cabc6d30d
simonyoung <simon180@mac.com>
2010-04-22 16:04:01 +0100

    Updated with base template (not IE tested)

---
+++ templates/assets/js/ie6.js
@@ -0,0 +1,17 @@
+/*============================================================
+
+Author: Simon Young
+http://simonyoung.net
+http://twitter.com/simon180
+
+IE6.JS 
+IE6-specific JavaScript functionality (mainly belatedPNG usage)
+
+(c) Simon Young, 2010. All rights reserved.
+
+// Fix PNG display in IE6
+//* Example usage:
+//* DD_belatedPNG.fix('.png_bg'); // argument is a CSS selector
+//* DD_belatedPNG.fixPng( someNode ); // argument is an HTMLDomElement
+
+============================================================*/
