7dcca8ad6e4b7f0972cc9ade68b6f7d4e369394c
simonyoung <simon180@mac.com>
2010-05-26 20:25:47 +0100

    Removed option to hide video on mouseover as it was minging.

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -30,16 +30,7 @@ $(document).ready(function () {
         });
     });
     
-    
     // initialise Superfish 
     $("ul.nav").superfish();
-    
-    $('#nav_training, #nav_apps').mouseover(function(){
-       $('video').hide();
-    });
-    
-    $('#nav_training, #nav_apps').mouseout(function(){
-       $('video').show();
-    });
     
 });
